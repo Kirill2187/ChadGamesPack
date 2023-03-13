23faee8c17f8450f38dcfe9d97249a9ea95cd785
Pavel Nikulin <pashenca8@mail.ru>
2023-03-13 23:24:16 +0300

    Minor fixes

--- core/src/main/java/com/chadgames/gamespack/utils/Constants.java
+++ core/src/main/java/com/chadgames/gamespack/utils/Constants.java
@@ -3,6 +3,4 @@ package com.chadgames.gamespack.utils;
 public final class Constants {
     private Constants() {}
 
-    public static final int TCP_PORT = 54555;
-    public static final int UDP_PORT = 54777;
 }
--- server/src/main/java/com/chadgames/gamespack/server/GameServer.java
+++ server/src/main/java/com/chadgames/gamespack/server/GameServer.java
@@ -48,7 +48,7 @@ public class GameServer {
         Network.registerClasses(server);
 
         server.start();
-        server.bind(Network.PORT); // TODO: magic constants
+        server.bind(Network.PORT);
         server.addListener(new Listener() {
             public void received(Connection connection, Object object) {
                 if (object instanceof Request) {
