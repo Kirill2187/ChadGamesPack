d672c2c5bee6c19ee13c87949a40d3a115bfd9e9
Kirill Pupkov <pupkovkirill@gmail.com>
2023-04-17 00:02:24 +0300

    Add empty constructor (essential for serialization)

--- core/src/main/java/com/chadgames/gamespack/games/chat/ChatMoveData.java
+++ core/src/main/java/com/chadgames/gamespack/games/chat/ChatMoveData.java
@@ -4,6 +4,7 @@ import com.chadgames.gamespack.games.MoveData;
 
 public class ChatMoveData extends MoveData {
     String message;
+    ChatMoveData() {}
     public ChatMoveData(int playerId, String message) {
         this.playerId = playerId;
         this.message = message;
