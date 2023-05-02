8a5c2bf4f74d42783c12fa1f89568a79690a7fe4
Kirill Pupkov <pupkovkirill@gmail.com>
2023-05-02 22:17:07 +0300

    Add tests

---
+++ core/src/test/java/com/chadgames/gamespack/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.chadgames.gamespack;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
---
+++ server/src/test/java/com/chadgames/gamespack/server/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.chadgames.gamespack.server;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
