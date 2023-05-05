5cd83650fb3df062ea6e71944f17fcee36aa320b
Kirill Pupkov <pupkovkirill@gmail.com>
2023-05-05 19:49:35 +0300

    Fix CE

--- core/src/main/java/com/chadgames/gamespack/games/reversi/ReversiRenderer.java
+++ core/src/main/java/com/chadgames/gamespack/games/reversi/ReversiRenderer.java
@@ -1,11 +1,28 @@
 package com.chadgames.gamespack.games.reversi;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.chadgames.gamespack.games.Actions;
 import com.chadgames.gamespack.games.GameProcess;
 import com.chadgames.gamespack.games.GameRenderer;
+import com.chadgames.gamespack.games.GameState;
 
 public class ReversiRenderer extends GameRenderer {
     public ReversiRenderer(GameProcess gameProcess, Table gameTable) {
-        super();
+        super(gameProcess);
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void makeActions(Actions actions) {
+
+    }
+
+    @Override
+    public void loadFromState(GameState gameState) {
+
     }
 }
